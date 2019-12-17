78500acfeecc6d2dd2c263c3caaab846be85d0cb
rjulien1994 <50495239+rjulien1994@users.noreply.github.com>
2019-12-17 18:21:27 -0500

    Update levels.js

--- static/js/levels.js
+++ static/js/levels.js
@@ -1,7 +1,9 @@
 function setLevel(game, lvl) {
   var monsters = [];
   Levels[lvl].forEach(ennemy => {
-    monsters.push(new littleShit(game, { x: ennemy.x, y: ennemy.y }));
+    if(ennemy.id == 1) {monsters.push(new littleShit(game, { x: ennemy.x, y: ennemy.y }))}
+    else if(ennemy.id == 2) {monsters.push(new fly(game, { x: ennemy.x, y: ennemy.y }))}
+    else if(ennemy.id == 3) {monsters.push(new evilGhost(game, { x: ennemy.x, y: ennemy.y }))}
   });
 
   return monsters;
@@ -9,15 +11,46 @@ function setLevel(game, lvl) {
 
 const Levels = [
   [], //level 0
-
   [
-    { id: 1, x: 200, y: 200 }, //level 1
-    { id: 1, x: 400, y: 200 }
+    { id: 1, x: 25, y: 20 }, //level 1
+    { id: 1, x: 75, y: 20 }
   ],
-
   [
-    { id: 1, x: 200, y: 200 }, //level 2
-    { id: 1, x: 400, y: 200 },
-    { id: 1, x: 600, y: 200 }
+    { id: 2, x: 25, y: 25 },  //level2
+    { id: 2, x: 75, y: 25 }
+  ],
+  [
+    { id: 3, x: 25, y: 25 },  //level3
+    { id: 3, x: 75, y: 25 }
+  ],
+  [
+    { id: 1, x: 75, y: 20 },  //level4
+    { id: 2, x: 35, y: 25 },
+    { id: 3, x: 75, y: 30 }
+  ],
+  [
+    { id: 2, x: 20, y: 50 },  //leve5
+    { id: 1, x: 35, y: 25 },
+    { id: 2, x: 50, y: 15 }, 
+    { id: 1, x: 65, y: 25 },
+    { id: 2, x: 80, y: 50 }
+  ],
+  [
+    { id: 3, x: 10, y: 10 },  //leve6
+    { id: 3, x: 90, y: 10 },
+    { id: 3, x: 10, y: 75 }, 
+    { id: 3, x: 90, y: 75 },
+    { id: 2, x: 10, y: 55 },
+    { id: 2, x: 90, y: 55 }
+  ],
+  [
+    { id: 1, x: 20, y: 20 },
+    { id: 1, x: 40, y: 20 },
+    { id: 1, x: 60, y: 20 }, //level 7
+    { id: 1, x: 80, y: 20 },
+    { id: 1, x: 20, y: 40 },
+    { id: 1, x: 40, y: 40 },
+    { id: 1, x: 60, y: 40 }, 
+    { id: 1, x: 80, y: 40 }
   ]
 ];
