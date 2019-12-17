87cf62b1d4d78e946d7e38465929733f360870da
rjulien1994 <50495239+rjulien1994@users.noreply.github.com>
2019-12-17 18:18:18 -0500

    Update doors.js

--- static/js/doors.js
+++ static/js/doors.js
@@ -3,18 +3,26 @@ class levelDoor {
     this.game = game;
     this.image = document.getElementById("deathDoor");
     this.size = {
-      x: (this.game.gameWidth * 10) / 100,
-      y: this.game.Limits.top + (1.5 * this.game.gameHeight) / 100
+      x: 10,
+      y: this.game.Limits.top + 1.5
     }; //sets size
     this.position = {
       //sets position
-      x: (50 * this.game.gameWidth) / 100 - this.size.x / 2,
-      y: -game.gameHeight / 100
+      x: 50 - this.size.x / 2,
+      y: -0.75
     };
   }
 
   update(dt) {
     if (Collision(this, this.game.player)) {
+      if(user.admin === 'True' && this.game.level === 0) {
+        document.getElementById('adminBoard').style.display = "none";
+        this.game.player.attack = document.getElementById("attackEdit").value;
+        this.game.player.maxSpeed = document.getElementById("speedEdit").value;
+        this.game.player.fireRate = document.getElementById("fireRateEdit").value;
+        this.game.player.range = document.getElementById("rangeEdit").value;
+        this.game.player.shotSpeed = document.getElementById("shotSpeedEdit").value;
+      }
       this.game.loadLevel();
     }
   }
@@ -22,10 +30,10 @@ class levelDoor {
   draw(ctx) {
     ctx.drawImage(
       this.image,
-      this.position.x,
-      this.position.y,
-      this.size.x,
-      1.3 * this.size.y
+      this.position.x * this.game.units.x,
+      this.position.y * this.game.units.y,
+      this.size.x * this.game.units.x,
+      1.3 * this.size.y  * this.game.units.y
     );
   }
 }
@@ -35,13 +43,13 @@ class goldenDoor {
     this.game = game;
     this.image = document.getElementById("goldDoor");
     this.size = {
-      x: this.game.Limits.left + this.game.gameWidth / 100,
-      y: (this.game.gameHeight * 18) / 100
+      x: this.game.Limits.left + 1,
+      y: 13.5
     }; //sets size
     this.position = {
       //sets position
       x: 0,
-      y: (50 * this.game.gameHeight) / 100 - this.size.y / 2
+      y: this.game.gameHeight/2 - this.size.y / 2
     };
   }
 
@@ -54,10 +62,10 @@ class goldenDoor {
   draw(ctx) {
     ctx.drawImage(
       this.image,
-      this.position.x,
-      this.position.y,
-      this.size.x,
-      this.size.y
+      this.position.x * this.game.units.x,
+      this.position.y * this.game.units.y,
+      this.size.x * this.game.units.x,
+      this.size.y * this.game.units.y
     );
   }
 }
@@ -67,20 +75,28 @@ class satanDoor {
     this.game = game;
     this.image = document.getElementById("satanDoor");
     this.size = {
-      x: this.game.gameWidth - this.game.Limits.right + game.gameWidth / 100,
-      y: (this.game.gameHeight * 18) / 100
+      x: this.game.gameWidth - this.game.Limits.right + 1,
+      y: 13.5
     }; //sets size
     this.position = {
       //sets position
       x: game.gameWidth - this.size.x,
-      y: (50 * this.game.gameHeight) / 100 - this.size.y / 2
+      y: this.game.gameHeight/2 - this.size.y / 2
     };
   }
 
   update(dt) {
     if (Collision(this, this.game.player)) {
-      if(this.game.level !== 0) {
-        document.getElementById("levelStats").submit();
+      if (this.game.level != 0) {
+        let parameters = {
+          attack: this.game.player.attack, 
+          speed: this.game.player.maxSpeed,
+          fireRate: this.game.player.fireRate,
+          range: this.game.player.range,
+          shotSpeed: this.game.player.shotSpeed,
+          phone: this.game.touchScreen
+        };
+       postData(parameters); 
       }
       this.game.resetGame();
     }
@@ -89,10 +105,10 @@ class satanDoor {
   draw(ctx) {
     ctx.drawImage(
       this.image,
-      this.position.x,
-      this.position.y,
-      this.size.x,
-      this.size.y
+      this.position.x  * this.game.units.x,
+      this.position.y * this.game.units.y,
+      this.size.x * this.game.units.x,
+      this.size.y * this.game.units.y
     );
   }
 }
